104d9a84bf9fab25d09b14095536adae189d5cd5
xinwei2 <xinwei2@iflytek.com>
2020-03-16 11:46:18 +0800

    [新增代码]获取存储权限

--- app/src/main/java/com/xinwei/richtexteditor/RichTextMainActivity.java
+++ app/src/main/java/com/xinwei/richtexteditor/RichTextMainActivity.java
@@ -1,13 +1,15 @@
 package com.xinwei.richtexteditor;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 
-
+import androidx.core.app.ActivityCompat;
 import com.xinwei.richtexteditor.utils.AssetUtils;
 
 import java.io.File;
@@ -21,10 +23,17 @@ public class RichTextMainActivity extends Activity {
 
     private static final String TAG = "RichTextMainActivity";
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rich_text_main);
+        verifyStoragePermissions(this);
 
         initData();
     }
@@ -68,5 +77,19 @@ public class RichTextMainActivity extends Activity {
                 }
             }
         }).start();
+    }
+
+    /**
+     * 获取存储权限
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
     }
 }
